d83d4682827308afa1afa50af1bea56f1c30119e
SpaceKoder <43577093+SpaceKoder@users.noreply.github.com>
2018-09-25 22:01:22 +0800

    Add files via upload

---
+++ Button Ripple Effect/js/main.js
@@ -0,0 +1,35 @@
+$(function(){
+
+  //creating a style object for the ripple effect
+  function RippleStyle(width, height, posX, posY){
+    this.width = ( width <= height ) ? height : width;
+    this.height = ( width <= height ) ? height : width;
+    this.top = posY - (this.height * 0.5);
+    this.left = posX - (this.width * 0.5);
+  }
+
+  $('.btn').on('mousedown', function(e){
+    //appending an element with a class name "btn-ripple"
+    var rippleEl = $('<span class="btn-ripple"></span>').appendTo(this);
+
+    //getting the button's offset position
+    var pos = $(this).offset();
+
+    //get the button's width and height
+    var width = $(this).outerWidth();
+    var height = $(this).outerHeight();
+
+    //get the cursor's x and y position within the button
+    var posX = e.pageX - pos.left;
+    var posY = e.pageY - pos.top;
+
+    //adding a css style to the ripple effect
+    var rippleStyle = new RippleStyle(width, height, posX, posY);
+    rippleEl.css(rippleStyle);
+  });
+
+  //this event listener will be triggered once the ripple animation is done
+  $('.btn').on('animationend webkitAnimationEnd oanimationend MSAnimationEnd', '.btn-ripple', function(){
+    $(this).remove();
+  });
+});
